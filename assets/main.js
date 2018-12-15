e86fb0cb467ceb25c21a400a5b33c48b7e5c4890
Emil Sroka <srokaemil@gmail.com>
2018-12-15 23:47:10 +0100

    Add star object

--- assets/main.js
+++ assets/main.js
@@ -25,6 +25,45 @@ addEventListener('mousemove', event => {
 })
 
 // Objects
+function Star(x, y, radius, isStatic) {
+    // position and movment
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    // look 
+    this.fillColor = "#ffffff";
+    this.strokeColor = "#777777";
+    this.isStatic = isStatic; // !!!!
+    // opacity
+    this.minOpacity = (Math.random() / 5) + 0.1;
+    this.maxOpacity = (Math.random() / 5) + 0.7;
+    this.currentOpacity = this.minOpacity;
+
+    this.update = function(){
+        // calc opacity
+        if (distance(this.x, this.y, mouse.x, mouse.y) <= 120 && this.currentOpacity < this.maxOpacity){
+            this.currentOpacity += 0.02;
+        } else if (this.currentOpacity > this.minOpacity) {
+            this.currentOpacity -= 0.02;
+        }
+        this.draw();
+    }
+
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        c.save();
+        c.globalAlpha = this.currentOpacity;
+        c.fillStyle = this.fillColor;
+        c.fill();
+        c.strokeStyle = this.strokeColor;
+        c.stroke();
+        c.restore();
+    };  
+
+}
+
+var newStar = new Star(100, 100, 2, true);
 
 // Implementation
 function init() {
@@ -36,7 +75,7 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
 
-    c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
+    newStar.update();
 }
 
 init()
