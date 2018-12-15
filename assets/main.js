fd774795f2df4922dac58276948d6d7d971f48af
Emil Sroka <srokaemil@gmail.com>
2018-12-15 23:44:36 +0100

    Add util functions
    Add distance and lerp

--- assets/main.js
+++ assets/main.js
@@ -4,6 +4,15 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+// Util functions
+function distance(x1, y1, x2, y2){
+    return Math.floor(Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)));
+}
+
+function lerp(v0, v1, t) {
+    return v0 + t * (v1 - v0);
+}
+
 const mouse = {
     x: innerWidth / 2,
     y: innerHeight / 2
