c11447dfb4c4f8819550d1a1449cd4c67d0ec8d2
Emil Sroka <srokaemil@gmail.com>
2018-12-18 00:14:22 +0100

    Add getDamage method

--- assets/main.js
+++ assets/main.js
@@ -99,15 +99,21 @@ function GameController() {
 }
 
 function Player(x, y, img) {
-    this.width = 120;// unit
-    this.height = 80;// unit
+    // movement and position (game unit)
+    this.width = 120;
+    this.height = 80;
     this.x = x-this.width/2;
     this.y = y-this.height-10;
     this.dx = 0;
     this.dxLimit = 20;
-    this.img = new Image(); this.img.src = img;
     this.inertness = false;
     this.inertnessTimer = 0;
+    // game mechanic
+    this.hp = 100;
+    this.damageEffect = false;
+    this.damageEffectTimer = 0;
+    this.img = new Image(); this.img.src = img;
+    
 
     this.update = function () {
         // calc velocity
@@ -134,10 +140,30 @@ function Player(x, y, img) {
             this.dx = -2 * Math.abs(this.dx);
             this.inertness = true;
         }
+        // !!!
+        let opacity = 1;
+        if(this.damageEffect){
+            if(this.damageEffectTimer < 500){
+                this.damageEffectTimer += gameTime.deltaTime;
+                opacity = (1/100000)*this.damageEffectTimer*(this.damageEffectTimer - 500) + 1; // Quadratic function, value from 1 to ~0.4
+            } else {
+                this.damageEffect = false;
+                this.damageEffectTimer = 0;
+            }
+        }
+
         // move and draw
         this.x += this.dx;
-        this.draw();
+        this.draw(opacity);
     };
+
+    this.getDamage = function (dmg) {
+        // update hp and hpBar
+        this.hp -= dmg;
+        gameController.updateHpBar(this.hp);
+        // start damage effect
+        this.damageEffect = true;
+    }
 
     this.draw = drawFunction;
     
